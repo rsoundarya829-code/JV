cb0c4038fe51b3e156d2693e78596daa7e276575
R Soundarya <rsoundarya829@gmail.com>
2026-05-13 12:39:43 +0530

    calculating area

---
+++ jvk/area.java
@@ -0,0 +1,10 @@
+package jvk;
+
+public class area {
+    public static void main(String args[]){
+        int r = 5;
+        double area = 3.14 * r * r;
+        System.out.println("Area of circle is: " + area);
+    }
+    
+}
