f9c281a9e282f571e59806084936c3c5ebf47fe1
R Soundarya <rsoundarya829@gmail.com>
2026-05-13 12:29:45 +0530

    hii

--- MAIN.java
+++ MAIN.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MAIN {
+public class MAIN{
     public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     int month = sc.nextInt();
